6a5471abbccc8887479ef44301c53c830e8bfa1b
pyncer <root@pyncer.com>
2025-08-08 01:18:47 -0400

    Updated for v1.3.2 release.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,12 @@
 # Change Log
 
+## 1.3.2 - 2025-08-08
+
+### Fixed
+
+- Added missing hasColumns method to ColumnsQueryInterface.
+- Removed ignored PHPStan rules.
+
 ## 1.3.1 - 2025-08-08
 
 ### Added
--- src/Record/ColumnsQueryInterface.php
+++ src/Record/ColumnsQueryInterface.php
@@ -6,4 +6,6 @@ use Pyncer\Database\Record\RecordQueryInterface;
 interface ColumnsQueryInterface extends RecordQueryInterface
 {
     public function columns(string|array ...$columns): static;
+
+    public function hasColumns(): bool;
 }
